3ad992797fb973c7163752256f26e1b8b44f743d
asemt <andreas.semt@gmx.net>
2016-06-08 13:30:20 +0200

    Add logstash heartbeat monitoring.

--- logstash_service.sh
+++ logstash_service.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Make sure logstashbrcvr is running before logstash starts.
+sv start logstashbrcvr || exit 1
+
+# Start logstash itself.
 cd /opt/logstash
+exec 2>&1
 exec /sbin/setuser logstash bin/logstash -f /etc/logstash/conf.d/logstash.conf
