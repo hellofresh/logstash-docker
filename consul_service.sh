f47f5862603b62434b8be02876260662cf56b850
asemt <andreas.semt@gmx.net>
2016-06-22 14:58:34 +0200

    Try to join that cluster.

--- consul_service.sh
+++ consul_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /usr/local/bin/consul agent -client 0.0.0.0 -config-dir /etc/consul.d -data-dir /var/consul/data -encrypt ${CONSUL_ENCRYPT} -join ${CONSUL_START_JOIN} -dc "${CONSUL_DATACENTER}"
+exec /usr/local/bin/consul agent -client 0.0.0.0 -bind 0.0.0.0 -config-dir /etc/consul.d -data-dir /var/consul/data -encrypt ${CONSUL_ENCRYPT} -dc=${CONSUL_DATACENTER} -join ${CONSUL_START_JOIN}
