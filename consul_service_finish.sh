20ab5a7e6221c3862649b89d09df490410e8874d
asemt <andreas.semt@gmx.net>
2016-06-24 10:07:34 +0200

    Make consul server address dynamic in force-leave HTTP call.

--- consul_service_finish.sh
+++ consul_service_finish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Extract one of the Consul servers to call the HTTP API.
+export CONSUL_HTTP_SRV="$(echo $CONSUL_START_JOIN | cut -d ' ' -f 1)"
+
 # Transition from "failed" to "left" state to prevent reconnects for 72h (as per default).
-curl -XGET "http://consul-server:8500/v1/agent/force-leave/$HOSTNAME"
+curl -XGET "http://$CONSUL_HTTP_SRV:8500/v1/agent/force-leave/$HOSTNAME"
