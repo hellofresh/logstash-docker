549177da615f87118bd689d2cb4590a2d71ba044
asemt <andreas.semt@gmx.net>
2016-06-24 11:31:11 +0200

    Explicitly set IP addr to bnd to.

--- consul_service.sh
+++ consul_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /usr/local/bin/consul agent -client 0.0.0.0 -bind 0.0.0.0 -config-dir /etc/consul.d -data-dir /var/consul/data -encrypt ${CONSUL_ENCRYPT} -dc=${CONSUL_DATACENTER} -join ${CONSUL_START_JOIN}
+exec /usr/local/bin/consul agent -client 0.0.0.0 -bind ${CONSUL_BIND_PRIVATE_IP} -config-dir /etc/consul.d -data-dir /var/consul/data -encrypt ${CONSUL_ENCRYPT} -dc=${CONSUL_DATACENTER} -join ${CONSUL_START_JOIN}
